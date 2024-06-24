57e080c456c24825bc2928470969eff7d6ead900
Karrington Gatlin <gatlinkarrington21@gmail.com>
2024-06-24 13:01:08 -0500

    git init

---
+++ basic-13/script.js
@@ -0,0 +1,59 @@
+/*
+The “Basic 13”
+These are Coding Dojo’s foundation “Basic 13” algorithm challenges. For each, write a Python function - a suggested function name is included below. Can you finish all of these challenges in less than two minutes each?   
+
+
+Print 1-255
+print_1_to_255()
+Print all the integers from 1 to 255. 
+
+3. Print Ints and Sum 0-255
+print_ints_and_sum_0_to_255()
+Print integers from 0 to 255, and with each integer print the sum so far. 
+
+5. Find and Print Max
+print_max_of_list(lst)
+Given a list, find and print its largest element. 
+
+7. List with Odds
+return_odds_list_1_to_255()
+Create a list with all the odd integers between 1 and 255 (inclusive).  
+
+9. Greater than Val
+greater_than_val(lst, val)
+Given a list and a value, count and print the number of list values greater than val. 
+
+11. Max, Min, Average
+max_min_average(lst)
+Given a list, print the max, min and average values for that list.
+
+13. Swap String For List Negative Values
+swap_string_for_negatives(lst)
+Given a list of integers or floats, replace any negative values with the string 'Dojo'.
+
+2. Print Odds 1-255
+print_odds_1_to_255()
+Print all odd integers from 1 to 255. 
+
+4. Iterate and Print List
+print_list_vals(lst)
+Iterate through a given list, printing each value. 
+
+6. Get and Print Average
+print_average_of_list(lst)
+Analyze a list’s values and print the average. 
+
+8. Square the Values
+square_list_vals(lst)
+Square each value in a given list, returning that same list with changed values. 
+
+10. Zero Out Negative Numbers
+zero_out_list_negatives(lst)
+Return the given list, after setting any negative values to zero. 
+
+12. Shift List Values
+shift_list_vals_left(lst)
+Given a list, move all values forward (to the left) by one index, dropping the first value and leaving a 0 (zero) value at the end of the list.
+
+
+*/
